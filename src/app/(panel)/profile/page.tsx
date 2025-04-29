ed7a159168d1101c38c9b5fffd9f069e0dead789
Leonardo Patrick <leo_viciogta@hotmail.com>
2025-04-29 14:16:42 -0300

    fix(profile/page): exibir detalhes do user conectado e cta para signout

--- src/app/(panel)/profile/page.tsx
+++ src/app/(panel)/profile/page.tsx
@@ -1,9 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useAuth } from "@/src/contexts/AuthContext";
+import { supabase } from "@/src/lib/supabase";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
 
 export default function Profile() {
+  const { setAuth, user } = useAuth();
+
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    setAuth(null);
+
+    if (error) {
+      Alert.alert("Error", "Erro ao sair da conta, tente mais tarde.");
+      return;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Page Perfil</Text>
+      <Text>Email: {user?.email}</Text>
+      <Text>Id: {user?.id}</Text>
+
+      <Button title="Deslogar" onPress={handleSignOut} />
     </View>
   );
 }
